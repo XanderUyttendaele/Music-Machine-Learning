995cdeb0432c792dc94972acb9fd2ae8d8e957b3
GwaihirtheWindlord <47426212+GwaihirtheWindlord@users.noreply.github.com>
2019-03-12 19:09:03 -0700

    Add files via upload
    Generates a spectrogram under Constant Q. Room for experimentation regarding parameters.

---
+++ GenerateSpectrogram.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import librosa
+import numpy as np
+
+
+def generateSpec(column_interval_sample, frequency_bins, bins_per_octave, file_path):
+    components, rate = librosa.load(file_path)
+    const_q_vals = np.abs(librosa.cqt(y = components, sr = rate, hop_length = column_interval_sample, n_bins = frequency_bins, bins_per_octave = bins_per_octave)).transpose() #Determine Constant Q values as per our above specifications. 
+    
+    plt.figure()
+    plt.imshow(np.abs(const_q_vals), aspect = "auto")
+    plt.plot()
+    
