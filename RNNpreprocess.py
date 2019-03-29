e5a4f463aca719b6dff05d13a8d48d0d4845bf7f
tenchipsofsalt <44712940+tenchipsofsalt@users.noreply.github.com>
2019-03-29 09:03:50 -0700

    Improved preprocessing to allow for stopping/starting again efficiently

--- RNNpreprocess.py
+++ RNNpreprocess.py
@@ -12,38 +12,39 @@ count = 0
 for root, directories, filenames in os.walk("E:\\musicdata\\Music-Machine-Learning\\song_data\\"):
     for filename in filenames:
         file_path = pt.join(root, filename)
-        base_path = pt.splitext(pt.basename(pt.normpath(file_path)))[0]        
-        
-        #Produce Constant Q Spectrogram
-        components, rate = librosa.load(root+"\\"+base_path+".wav")
-        const_q_vals = np.abs(librosa.cqt(y = components, sr = rate, hop_length = column_interval_sample, n_bins = frequency_bins, bins_per_octave = bins_per_octave)) #Determine Constant Q values as per our above specifications.
+        base_path = pt.splitext(pt.basename(pt.normpath(file_path)))[0]
         if(not pt.isfile("E:\\musicdata\\Music-Machine-Learning\\song_data_labeled\\" + base_path + ".npy")):
+        
+            #Produce Constant Q Spectrogram
+            components, rate = librosa.load(root+"\\"+base_path+".wav")
+            const_q_vals = np.abs(librosa.cqt(y = components, sr = rate, hop_length = column_interval_sample, n_bins = frequency_bins, bins_per_octave = bins_per_octave)).transpose() #Determine Constant Q values as per our above specifications.
+            
             np.save("E:\\musicdata\\Music-Machine-Learning\\song_data_training\\" + base_path, const_q_vals)
-        
-        #Each chord label will be represented by the frames of the spectrogram and the number of possible notes
-        num_spec_cols = const_q_vals.shape[1]
-        sampling_vec = column_interval_sample*np.arange(num_spec_cols)/float(rate)
-        note_label = np.zeros((num_spec_cols,num_notes))
-        
-        #Load our label array with provided file data
-        note_file = open(root+"\\"+base_path+".txt")
-        for line in note_file:
-            line_data = line.split()
-            if(line_data[0] != "OnsetTime"):
-                begin = float(line_data[0])
-                end = float(line_data[1])
-                mini = 0
-                maxi = len(sampling_vec)-1
-                while(begin > sampling_vec[mini]):
-                    mini+= 1
-                while(end < sampling_vec[maxi]):
-                    maxi-= 1
-                note_pitch = int(line_data[2]) - 21 
-                note_label[mini:maxi, note_pitch] = 1
-        note_file.close()
-        if(not pt.isfile("E:\\musicdata\\Music-Machine-Learning\\song_data_labeled\\" + base_path + ".npy")): 
-            np.save("E:\\musicdata\\Music-Machine-Learning\\song_data_labeled\\" + base_path, note_label)
+            #Each chord label will be represented by the frames of the spectrogram and the number of possible notes
+            num_spec_cols = const_q_vals.shape[0]
+            sampling_vec = column_interval_sample*np.arange(num_spec_cols)/float(rate)
+            note_label = np.zeros((num_spec_cols,num_notes))
+            
+            #Load our label array with provided file data
+            note_file = open(root+"\\"+base_path+".txt")
+            for line in note_file:
+                line_data = line.split()
+                if(line_data[0] != "OnsetTime"):
+                    begin = float(line_data[0])
+                    end = float(line_data[1])
+                    mini = 0
+                    maxi = len(sampling_vec)-1
+                    while(begin > sampling_vec[mini]):
+                        mini+= 1
+                    while(end < sampling_vec[maxi]):
+                        maxi-= 1
+                    note_pitch = int(line_data[2]) - 21 
+                    note_label[mini:maxi, note_pitch] = 1
+            note_file.close()
+            if(not pt.isfile("E:\\musicdata\\Music-Machine-Learning\\song_data_labeled\\" + base_path + ".npy")): 
+                np.save("E:\\musicdata\\Music-Machine-Learning\\song_data_labeled\\" + base_path, note_label)
         
         count+=1
+        print(count)
         if(count%100 == 0):
             print(count)
