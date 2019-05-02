883e9b59f36d3c70ceb6148ae594a306d68c726c
tenchipsofsalt <44712940+tenchipsofsalt@users.noreply.github.com>
2019-05-02 09:22:29 -0700

    Improvements

--- testModel.py
+++ testModel.py
@@ -18,7 +18,7 @@ import copy
 # DONE: Shift all offsets such that first note starts on beat one?
 # TODO: Dynamics, use accents to figure out time signature and start note?
 timeStep = 512/22050.0
-lengthToIgnore = 0 # ignore notes of this length or shorter - depending on length, can cause notes of length 0 after
+lengthToIgnore = 10 # ignore notes of this length or shorter - depending on length, can cause notes of length 0 after
 # shifting
 gapLength = 2
 noteList = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"]
@@ -68,8 +68,9 @@ def closest(input, step):
     return multiple * step
 
 sess = tf.Session()
-saver = tf.train.import_meta_graph("E:\\musicdata\\Music-Machine-Learning\\saved_models\\biLSTM\\model_biLSTM.ckpt.meta")
-saver.restore(sess, "E:\\musicdata\\Music-Machine-Learning\\saved_models\\biLSTM\\model_biLSTM.ckpt")
+saver = tf.train.import_meta_graph("E:\\musicdata\\Music-Machine-Learning\\saved_models\\biLSTM\\model_biLSTM128_50_3.0"
+                                   ".ckpt.meta")
+saver.restore(sess, "E:\\musicdata\\Music-Machine-Learning\\saved_models\\biLSTM\\model_biLSTM128_50_3.0.ckpt")
 
 print("Model restored.")
 graph = tf.get_default_graph()
@@ -92,10 +93,14 @@ for i in range(length//stepCount):
     output = sess.run(op, feed_dict)
     for j in range(stepCount):
         songData[i*stepCount + j] = output[0][j]
-    feed_dict = {x:[song[i * stepCount + offset: (i+1) * stepCount + offset]]}
-    output = sess.run(op, feed_dict)
-    for j in range(stepCount):
-        songData[i*stepCount + j + offset] = output[0][j]
+    # feed_dict = {x:[song[i * stepCount + offset: (i+1) * stepCount + offset]]}
+    # output = sess.run(op, feed_dict)
+    # for j in range(stepCount):
+    #     songData[i*stepCount + j + offset] = output[0][j]
+feed_dict = {x:[song[i * stepCount: (i+1) * stepCount]]}
+output = sess.run(op, feed_dict)
+for j in range(stepCount):
+    songData[-1 * (stepCount - j)] = output[0][j]
 print("Done processing " + str(length) + " timesteps")
 print("Converting to start/end format") # list of lists in format [note, start time, stop time]
 processedSongData = []
@@ -108,20 +113,15 @@ while i < length:
         if songData[i][j]:
             noteLength = 0
             iterator = i
-            count = 0
-            while songData[iterator][j]:
-                if not songData[iterator][j]:
-                    if count > int(lengthToIgnore//2) and True in songData[iterator+1:iterator+lengthToIgnore//3+ 1][j]:
-                        print("Bridging gap!")
-                        count = 0
-                        noteLength +=1
-                        iterator +=1
-                        continue
-                else:
+            tryContinue = False
+            while songData[iterator][j] or tryContinue:
+                tryContinue = False # change to True to enable gap fixing
+                if songData[iterator][j] or tryContinue:
+                    if not songData[iterator][j]:
+                        tryContinue = False
                     songData[iterator][j] = False
                     noteLength+=1
                     iterator+=1
-                    count+=1
             # code for fixing gaps, probably not a good idea (in tests, it messed up a lot of notes that should be
             # separate)
             # if noteLength != 0:
@@ -153,9 +153,9 @@ for b in range(length):
             off.append(processedSongData[c][0])
     startCounts.append(len(on))
     endCounts.append(len(off))
-playback = input("Play predictions? (y/n)")
-if playback == 'y':
-    play()
+# playback = input("Play predictions? (y/n)")
+# if playback == 'y':
+#     play()
 print("Beginning beat detection")
 tt = pylab.arange(len(startCounts))
 durations = pylab.arange(1.1,30,.02) # avoid 1.0
@@ -169,36 +169,64 @@ transform = pylab.array([fourier_transform(startCounts,d, tt) for d in durations
 #     top_k = np.sort(top_k)
 # # should be getting the larger of the 2
 # quarter_duration = int(round(durations[top_k[1]]))
+precision = 2 #int(round(top_k[1]/top_k[0])) # how many parts to split a quarter note into
 quarter_duration = int(round(durations[pylab.argmax(abs(transform))]))
-
+if 60.0/(quarter_duration * timeStep) > 180:
+    quarter_duration *= 2
+    precision *= 2
+elif 60.0/(quarter_duration * timeStep) < 60:
+    quarter_duration /= 2
+    precision /= 2
+# won't always be 2, should be 3 in some cases, but not gonna implement that atm
+precision = int(precision)
 pylab.plot(durations, abs(transform))
 pylab.xlabel('period (in timesteps)')
 pylab.ylabel('Spectrum value')
-pylab.show()
+# pylab.show()
 print("Estimated tempo (bpm): " + '%.3f' % (60.0/(quarter_duration * timeStep)))
 
 print("Shifting notes to tempo")
-precision = 2 #int(round(top_k[1]/top_k[0])) # how many parts to split a quarter note into
 print("Shifting notes to 1/" + str(precision) + " of a quarter")
 length *= precision
 timeStep /= precision
+shortestDuration = quarter_duration
 quarter_duration *= precision
 processedSongData = [[note[0],note[1]*precision,note[2]*precision]for note in processedSongData]
 shift = 0
 for note in processedSongData:
     temp = note[1]
     temptwo = note[2]
-    note[1] = note[1] - note[1] % (quarter_duration/precision)
-    note[2] = note[2] + quarter_duration/precision - note[2] % (quarter_duration/precision)
-    # note[2] = closest(note[2], quarter_duration/precision)
-    # int((note[1]//(quarter_duration/precision))*(quarter_duration/precision)) does it make more sense to use closest or just floor - if there's
+    if note[1] % (shortestDuration) > 3 * (shortestDuration) / 4:
+        note[1] += shortestDuration - (note[1] % shortestDuration)
+    else:
+        note[1] -= note[1] % shortestDuration
+    if note[2] % shortestDuration < shortestDuration / 4:
+        note[2] -= note[2] % shortestDuration
+    else:
+        note[2] += shortestDuration - (note[2] % shortestDuration)
+    if note[2] - note[1] > 4 * shortestDuration: # anything larger than this doesn't usually happen (e.g. a note
+    # with length of five sixteenths
+        if precision == 4 and note[2] - note[1] <= 8 * shortestDuration: # allowing dotted quarters (only regular
+            # note without quarter note multiple length
+            note[2] = note[1] + closest(note[2] - note[1], 2 * shortestDuration)
+        else:
+            note[2] = note[1] + closest(note[2] - note[1], quarter_duration)
+    if note[2] - note[1] >= 4 * shortestDuration:
+        if not int(round(note[1] / shortestDuration)) % 2 == 1:
+            note[1] -= shortestDuration
+            note[2] -= shortestDuration
+    # note[1] = closest(note[1], shortestDuration)
+    # note[1] = note[1] - note[1] % (shortestDuration)
+    # note[2] = note[2] + shortestDuration - note[2] % (shortestDuration)
+    # note[2] = closest(note[2], shortestDuration)
+    # int((note[1]//(shortestDuration))*(shortestDuration)) does it make more sense to use closest or just floor - if there's
     # a note in the middle of two 16ths or w/e do we assume it started at the previous 16th?
     shift += note[1] - temp
     shift += note[2] - temptwo
     if note[2] == note[1]:
-        note[2] += quarter_duration/precision
+        note[2] += shortestDuration
         print("Fixed 0 length note " + str(note[0]) + " at " + str(note[1]/(quarter_duration)))
-    # int((note[2]//(quarter_duration/precision))*(quarter_duration/precision))
+    # int((note[2]//(shortestDuration))*(shortestDuration))
     shift += note[2] - temp
 shift /= (len(processedSongData)*2)
 shift *= timeStep
@@ -209,9 +237,9 @@ for note in processedSongData:
     if lastPosNotes[note[0]] > note[1]:
         note[1] = lastPosNotes[note[0]]
     lastPosNotes[note[0]] = note[2]
-playback = input("Play predictions? (y/n)")
-if playback == 'y':
-    play()
+# playback = input("Play predictions? (y/n)")
+# if playback == 'y':
+#     play()
 print("Beginning sheet music generation - generating notes")
 tempoMarking = music21.tempo.MetronomeMark(number=int(60.0/(quarter_duration * timeStep)))
 score = music21.stream.Score()
